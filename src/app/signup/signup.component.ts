0f9ba1478b612356691e885d2378eb0a6be078c5
Zain Ali <zain0581@elev.tec.dk>
2023-02-16 14:32:44 +0100

    d.16

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+
+
+
+  type:string = "password";
+  istext:boolean = false;
+  eyeIcon:string="fa-eye-slash";
+
+
+  signupform!:FormGroup;
+  /**
+   *
+   */
+  constructor(private fb:FormBuilder) {}
+  ngOnInit():void{
+    this.signupform = this.fb.group(
+      {
+  
+        FirstName:['',Validators.required],
+        LastName:['',Validators.required],
+        UserName:['',Validators.required],
+        Email:['',Validators.required],
+        Password:['',Validators.required]
+       
+      }
+    )
+  
+    
+  }
+
+
+  hideshowpass(){
+    this.istext=!this.istext;
+    this.istext ? this.eyeIcon="fa-eye" : this.eyeIcon="fa-eye-slash";
+    this.istext ? this.type="text": this.type="password";
+
+}
+
+
+onSignup(){
+  if(this.signupform.valid){
+    console.log(this.signupform.value) }
+    else{
+      alert("Invalid Form")
+    }
+}
+
 
 }
